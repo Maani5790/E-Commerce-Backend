ec27c5553e4c504e37d38bb3d93ded3e8cdd8cb1
Abdul Rehman <arboacha@gmail.com>
2023-12-05 17:06:29 +0500

    update project

---
+++ database/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connection = async ( username,password ) =>{
+    const URL = `mongodb+srv://${username}:${password}@backend.ghrrl7r.mongodb.net/?retryWrites=true&w=majority`;
+    try{
+      await  mongoose.connect(URL);
+      console.log("Database connected Successfully")
+
+    } catch(error){
+        console.log("error while connected mongoDB", error)
+    }
+}
+
+export default  connection
---
+++ models/user.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "name is required"]
+    },
+    email: {
+        type: String,
+        required: [true, "email is required"]
+    },
+    password: {
+        type: String,
+        required: [true, "password is required"],
+        minLength: [6, "password length should be greater than 6"]
+    },
+    address: {
+        type: String,
+        required: [true, "address is required"]
+    },
+    city: {
+        type: String,
+        required: [true, "city is required"]
+    },
+    country: {
+        type: String,
+        required: [true, "country is required"]
+    },
+    phone: {
+        type: String,
+        required: [true, "city is required"]
+    },
+    profilePic: {
+        type: String,
+    },
+
+}, { Timestamp: true });
+
+export const userMdoel = mongoose.model("Users", userSchema);
+export default userMdoel;
