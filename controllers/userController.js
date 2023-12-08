f45fb2ac6dce8f51472e01ad5f89a2f7ad5ee2c7
Abdul Rehman <arboacha@gmail.com>
2023-12-08 19:59:09 +0500

    changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -40,18 +40,50 @@ export const registerController = async (req, res) => {
     };
 };
 
-export const loginController = (req, res, next) => {
+export const loginController = async (req, res) => {
     try {
+        const { email, password } = req.body;
 
+        if (!email || !password) {
+            return res.status(500).send({
+                success: false,
+                message: "Please Add Email OR Password",
+            });
+        }
+
+        const user = await userModel.findOne({ email });
+
+        if (!user) {
+            return res.status(404).send({
+                success: false,
+                message: "USer Not Found",
+            });
+        }
+
+        const isMatch = await user.comparePassword(password);
+
+        if (!isMatch) {
+            return res.status(500).send({
+                success: false,
+                message: "invalid credentials",
+            });
+        }
+        res
+            .status(200)
+            .send({
+                success: true,
+                message: "Login Successfully",
+                user,
+            });
     } catch (error) {
         console.log(error);
         res.status(500).send({
-            success: false,
-            message: "error in login user user cant exist",
-            error
-        })
+            success: "false",
+            message: "Error In Login Api",
+            error,
+        });
     }
-}
+};
 
 
 export default { registerController, loginController }
