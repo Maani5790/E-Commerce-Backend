623815b0fd575040ed6c6ac286ad65eb92536976
Abdul Rehman <arboacha@gmail.com>
2023-12-08 16:15:37 +0500

    hash

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = mongoose.Schema({
     name: {
@@ -36,5 +37,12 @@ const userSchema = mongoose.Schema({
 
 }, { Timestamp: true });
 
+userSchema.pre("save", async function () {
+    this.password = await bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.comparePassword = async function (plainPassword) {
+    return await bcrypt.compare(plainPassword, this.password);
+}
 const userModel = mongoose.model("Users", userSchema);
 export default userModel;
